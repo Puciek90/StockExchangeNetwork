9c9f632607150d061fc91e9efbe6fdc13810a007
Mateusz <mateusz.pluciennik93@gmail.com>
2016-11-13 17:06:27 +0100

    Added NetworkLayer constructor

--- src/main/java/networkLayer/NetworkLayer.java
+++ src/main/java/networkLayer/NetworkLayer.java
@@ -1,4 +1,19 @@
 package networkLayer;// Created by Mateusz Płuciennik on 13.11.16.
 
+import neuralCell.NeuralCell;
+
+import java.util.ArrayList;
+import java.util.List;
+
 public class NetworkLayer {
+    private List<NeuralCell> neuralCells;
+
+    public NetworkLayer(int cellsNumber, int neuralCellsInput) {
+        this.neuralCells = new ArrayList<NeuralCell>(cellsNumber);
+
+        for (int i = 0; i< cellsNumber; i++) {
+            neuralCells.set(i, new NeuralCell(neuralCellsInput));
+        }
+    }
+
 }
